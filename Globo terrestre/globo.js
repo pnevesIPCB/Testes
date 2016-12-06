4ca54d9f4f97013a665f8c7cc607aacd43bd551f
Paulo Alexandre Neves <paulo.alex.neves@gmail.com>
2016-12-06 20:59:55 +0000

    Added promise to earth load texture

--- Globo terrestre/globo.js
+++ Globo terrestre/globo.js
@@ -12,10 +12,32 @@
     var spotLight;
     var earthMesh;
     var position = {x: 0, y:0, z:0};
+    var keyboarder;
+    var earthMaterial;
 
-    var somethingToDo = true;
+    var earthPosition ={x:0, y:0, z:0};
 
     var stats = new Stats();
+
+    var promise = new Promise (function (resolve, reject){
+        earthTexture = new THREE.TextureLoader();
+        earthTexture.load('earthmap4k.jpg',
+            function ( texture ) {
+                // do something with the texture
+                earthMaterial = new THREE.MeshBasicMaterial();
+                earthMaterial.map = texture;
+                resolve ("success");
+            },
+            // Function called when download progresses
+            function ( xhr ) {
+                console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+            },
+            // Function called when download errors
+            function ( xhr ) {
+                reject("not loaded");
+            });
+    });
+
 
     var guiValues = new function(){
         this.cameraX = 30;
@@ -32,6 +54,8 @@
     function init() {
         scene = new THREE.Scene();
 
+        keyboarder = new Keyboarder();
+
         gui = new dat.GUI();
 
         renderer = new THREE.WebGLRenderer();
@@ -44,7 +68,7 @@
             45, window.innerWidth / window.innerHeight, 0.1, 1000);
         //35, 36, 33
 
-        //camera.position.set (30, 30, 30);
+        camera.position.set (30, 30, 30);
 
         /*
         camera.position.x = 35;
@@ -54,7 +78,10 @@
 
         stats.showPanel (0);
 
-        //cameraControl = new THREE.OrbitControls (camera, renderer.domElement);
+        cameraControl = new THREE.OrbitControls (camera, renderer.domElement);
+
+
+        camera.lookAt (scene.position);
 
         window.addEventListener('resize', function() {
             var WIDTH = window.innerWidth,
@@ -64,6 +91,7 @@
             camera.updateProjectionMatrix();
         });
 
+        /*
         var camGUIFolder = gui.addFolder ('Camera');
 
         camGUIFolder.add (guiValues, 'cameraX',-100, 100).step (1);
@@ -76,20 +104,32 @@
         lightGUIFolder.add (guiValues, 'lightPosY',-100, 100).step (1);
         lightGUIFolder.add (guiValues, 'lightPosZ',-100, 100).step (1);
 
+        */
+
+        var sphereGeometry = new THREE.SphereGeometry(15, 30, 30);
+
+        promise.then (function(result) {
+            console.log(result); // Correu bem!
+            earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
+            earthMesh.name = 'earth';
+            scene.add(earthMesh);
+            render();
+        }, function(err) {
+            console.log(err); // Erro!!!
+        });
+
+
         document.body.appendChild( stats.dom );
 
         document.body.appendChild(renderer.domElement);
+
+
 
     }
 
     function draw(){
 
-        var sphereGeometry = new THREE.SphereGeometry(15, 30, 30);
-        //var sphereMaterial = new THREE.MeshNormalMaterial();
-        var sphereMaterial = new THREE.MeshLambertMaterial({color: 0x00FF00});
-        earthMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
-        earthMesh.name = 'earth';
-        scene.add(earthMesh);
+
 
         spotLight = new THREE.SpotLight(0xffffff);
         spotLight.position.set(guiValues.lightPosX, guiValues.lightPosY, guiValues.lightPosZ);
@@ -103,12 +143,14 @@
         // render using requestAnimationFrame
         //cameraRot(guiVariables.rotSpeed);
 
-        //cameraControl.update();
+        cameraControl.update();
 
-        camera.position.set (guiValues.cameraX, guiValues.cameraY, guiValues.cameraZ);
-        spotLight.position.set(guiValues.lightPosX, guiValues.lightPosY, guiValues.lightPosZ);
+        //camera.position.set (guiValues.cameraX, guiValues.cameraY, guiValues.cameraZ);
+        //spotLight.position.set(guiValues.lightPosX, guiValues.lightPosY, guiValues.lightPosZ);
 
         updateScene();
+
+
 
         camera.lookAt(scene.position);
         renderer.render(scene, camera);
@@ -120,18 +162,48 @@
     //testing some "game" functionality
     function updateScene(){
 
-        position.x +=0.1;
+        if (keyboarder.isDown(keyboarder.KEYS.LEFT))
+            earthPosition.x -= 0.1;
 
-        earthMesh.position.set (position.x, position.y, position.z);
+        earthMesh.position.set (earthPosition.x, earthPosition.y, earthPosition.z);
     }
 
     window.onload = function(){
         init();
         draw();
-
-        render();
     };
 
 
+    var Keyboarder = function(){
+        var keyState = {};
+        window.onkeydown = function (e){
+            keyState[e.keyCode] = true;
+        };
+
+        window.onkeyup = function (e){
+            keyState[e.keyCode] = false;
+        };
+
+        this.isDown = function (keyCode){
+            return keyState[keyCode] === true;
+        };
+
+        this.isUp = function (keyCode){
+            return keyState[keyCode] === false;
+        };
+
+        this.wasToggled = function (keyCode){
+
+
+        };
+        this.KEYS = { LEFT: 37, RIGHT: 39, SPACE: 32, PAUSE: 80};
+
+        //toggle state
+        // 0 not on
+        //1 on
+        //2 toggled
+        //this.STATE = {left = false}
+
+    };
 
 })();
